d2b654d85043b06541d45dc0dfde45513199a473
GregFa <faragegr@gmail.com>
2023-03-31 12:27:36 -0500

    updated bash script

--- BulkLMM_API/example/cmdscansys.sh
+++ BulkLMM_API/example/cmdscansys.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # chmod u+x cmdtest.sh
-julia --project --sysimage=BulkLMM_API.so scan.jl data/bxdData/spleen-pheno-nomissing.csv 1112 data/bxdData/spleen-bxd-genoprob.csv
+julia --project --sysimage=bin/BulkLMM_API.so bin/single_trait_scan.jl data/preprocessed/single_trait.csv data/preprocessed/geno.csv data/preprocessed/kinship.csv data/output/lod_1.csv 1000 
