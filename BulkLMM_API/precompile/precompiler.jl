8bce3030c5002b0174d2e8da60633487370421a3
GregFa <faragegr@gmail.com>
2023-03-31 11:56:56 -0500

    moved bash files to example folder

--- BulkLMM_API/precompile/precompiler.jl
+++ BulkLMM_API/precompile/precompiler.jl
@@ -1,10 +1,10 @@
-using BulkLMM
+using BulkLMM, CSV, Tables
+
 bulklmmdir = dirname(pathof(BulkLMM));
+
 include(realpath(joinpath(bulklmmdir, "..", "test", "runtests.jl")))
 
-# TODO add CVS write and TABLEs
-
-# geno_file = joinpath(bulklmmdir,"..","data/bxdData/spleen-bxd-genoprob.csv");
-# geno = BulkLMM.DelimitedFiles.readdlm(geno_file, ',', header = false);
-# geno_processed = geno[2:end, 1:2:end] .* 1.0;
-# knshp = calcKinship(geno_processed); 
+mat = rand(2,2);
+path_file = joinpath(@__DIR__, "tmp.csv")
+CSV.write(path_file, Tables.table(mat), header = false);
+rm(path_file, force = true)
